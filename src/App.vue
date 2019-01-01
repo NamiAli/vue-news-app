48cccc6a5726154ee04e5de684d257f1fa1b9f9e
Nami Malik <1711206138.qq.com>
2018-12-31 19:34:33 -0800

    News App

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class='container' id="app">
+   <SourceSelection v-on:sourceChanged="sourceChangeHandler"></SourceSelection>
+   <NewsList v-bind:source="source"></NewsList>
+  </div>
+</template>
+
+<script>
+
+import SourceSelection from './components/SourceSelection'
+import NewsList from './components/NewsList';
+
+export default {
+  name: 'App',
+  components: {
+    SourceSelection,
+    NewsList
+  },
+
+  data(){
+    return {
+        source: ''
+    }
+  }, 
+
+  methods: {
+    sourceChangeHandler: function(source){
+       this.source = source
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+         padding-top:20px;
+  }
+</style>
